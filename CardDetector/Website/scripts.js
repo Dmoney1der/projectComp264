de317b29ec79f93e2bb106d2c376a837e85ccc00
Devonte H <90941121+Dmoney1der@users.noreply.github.com>
2023-04-12 12:01:52 -0400

    Update scripts.js

--- CardDetector/Website/scripts.js
+++ CardDetector/Website/scripts.js
@@ -1,9 +1,80 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+"use strict"
+const serverUrl = "htpp://http://127.0.0.1:8000"
+async function uploadImage() {
+    // encode input file as base64 string for upload
+    let file = document.getElementById("file").files[0];
+    let converter = new Promise(function(resolve, reject) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result
+            .toString().replace(/^data:(.*,)?/, ''));
+        reader.onerror = (error) => reject(error);
+    });
+    let encodedString = await converter;
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render( < App tab = "home" / > );
+    // clear file upload input field
+    document.getElementById("file").value = "";
+
+    // make server call to upload image
+    // and return the server upload promise
+    return fetch(serverUrl + "/images", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ filename: file.name, filebytes: encodedString })
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new HttpError(response);
+        }
+    })
+}
+
+function updateImage(image) {
+    document.getElementById("view").style.display = "block";
+
+    let imageElem = document.getElementById("image");
+    imageElem.src = image["fileUrl"];
+    imageElem.alt = image["fileId"];
+
+    return image;
+}
+
+function OCR(image) {
+    // make server call to translate image
+    // and return the server upload promise
+    return fetch(serverUrl + "/images/" + image["fileId"] + "/recognize_entities", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ fromLang: "auto", toLang: "en" })
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new HttpError(response);
+        }
+    })
+}
+
+function uploadCard() {
+    uploadImage()
+        .then(image => updateImage(image))
+        .then(image => OCR(image))
+        .catch(error => {
+            alert("Error: " + error);
+        })
+}
+
+class HttpError extends Error {
+    constructor(response) {
+        super(`${response.status} for ${response.url}`);
+        this.name = "HttpError";
+        this.response = response;
+    }
+}
